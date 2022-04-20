62e1f1a20c6103fe2f33af60106508b37ed52c46
Vanno221 <tommaso.vanno@gmail.com>
2022-04-20 14:53:28 +0200

    Setup enviroment

---
+++ app.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
